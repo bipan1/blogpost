411a504305ebf6df02f0b55dfce773f526789799
bipan <bipan13345@gmail.com>
2024-12-06 22:14:30 +1100

    Bug fixed

--- src/lib/QuillImageUpload.ts
+++ src/lib/QuillImageUpload.ts
@@ -12,30 +12,32 @@ const s3 = new AWS.S3();
 export const handleImageUpload = (
   quillRef: React.MutableRefObject<ReactQuill | null>,
 ) => {
-  const input = document.createElement('input');
-  input.setAttribute('type', 'file');
-  input.setAttribute('accept', 'image/*');
-  input.click();
+  if (typeof document !== 'undefined') {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
 
-  input.onchange = async () => {
-    if (input.files && input.files[0]) {
-      const file = input.files[0];
-      const params = {
-        Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME!,
-        Key: `${Date.now()}-${file.name}`,
-        Body: file,
-        ContentType: file.type,
-      };
+    input.onchange = async () => {
+      if (input.files && input.files[0]) {
+        const file = input.files[0];
+        const params = {
+          Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME!,
+          Key: `${Date.now()}-${file.name}`,
+          Body: file,
+          ContentType: file.type,
+        };
 
-      try {
-        const { Location } = await s3.upload(params).promise();
-        if (quillRef.current) {
-          const quill = quillRef.current.getEditor();
-          const range = quill.getSelection(true);
-          quill.insertEmbed(range.index, 'image', Location);
+        try {
+          const { Location } = await s3.upload(params).promise();
+          if (quillRef.current) {
+            const quill = quillRef.current.getEditor();
+            const range = quill.getSelection(true);
+            quill.insertEmbed(range.index, 'image', Location);
+          }
+        } catch (error) {
+          console.error('Error uploading file:', error);
         }
-      } catch (error) {
-        console.error('Error uploading file:', error);
       }
     }
   };
